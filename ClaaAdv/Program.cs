797176a7dc916d6bdb52731f308dd4de69fa96d6
Chaeyoung Lim <rnldyal1027@naver.com>
2023-05-25 09:06:10 +0900

    소멸자 실습

--- ClaaAdv/Program.cs
+++ ClaaAdv/Program.cs
@@ -1,22 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace ClaaAdv
+namespace ClassAddr
 {
-    internal static class Program
+    internal class Program
     {
-        /// <summary>
-        /// 해당 애플리케이션의 주 진입점입니다.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            // 소멸자 예시
+            Product product1 = new Product("과자", 1000);
+            Product product2 = new Product("아이스크림", 1000);
+            Product product3 = new Product("케이크", 1000);
         }
     }
 }
